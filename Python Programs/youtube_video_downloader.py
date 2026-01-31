01b2b114f0a1a710978c9ed18b19bcda06d93178
NithinKumar9 <nithinbyadav999@gmail.com>
2026-01-31 15:26:53 +0530

    youtube video downloader

---
+++ Python Programs/youtube_video_downloader.py
@@ -0,0 +1,18 @@
+# Import the library 
+# "pip install yt-dlp"
+
+#code
+
+import yt_dlp
+def download_yt_video(url):
+    ydl_opts = {
+        "format" : 'bestvideo[height<=1080]',
+        'noplaylist': True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    
+if __name__ == "__main__":
+    video_url = input("Enter the Youtube video URL: ")
+    download_yt_video(video_url)
