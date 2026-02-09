e78beb37ef2f443f73ec6f44b7d0228e8abb29fc
NithinKumar9 <nithinbyadav999@gmail.com>
2026-02-09 19:13:17 +0530

    upload

---
+++ Python Programs/printing_tables_from_user_input.py
@@ -0,0 +1,21 @@
+number = int(input("Enter the number to print the table: "))
+
+i = 1
+while i <= 10:
+    print(f"{number}X{i}={number*i}")
+    i+=1
+
+
+
+# Output:
+# Enter the number to print the table: 5
+# 5X1=5
+# 5X2=10
+# 5X3=15
+# 5X4=20
+# 5X5=25
+# 5X6=30
+# 5X7=35
+# 5X8=40
+# 5X9=45
+# 5X10=50
